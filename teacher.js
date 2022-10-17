8d6903ebd586ed27800327d74b19016ff9ee0652
TianYufan <3304216697@qq.com>
2022-10-17 18:53:03 +0800

    change

--- teacher.js
+++ teacher.js
@@ -1,15 +1,11 @@
-// 获取提交状态
-        function getstate(){
-            $.ajax({
-                //${pageContext.request.contextPath}用于取后端方法的绝对路径的项目名
-                url: "${pageContext.request.contextPath}/user/returnJson",
-                type: "GET",
-                data:{state: $("#state").val()},//必须是字符串格式
-                contentType:"application/json", //指定内容格式
-                dataType:json,
-                success: function(data) {  //括号里的data是服务器返回的数据
-                    console.log(data);
-    
-                }
-            });
-        }
+function show() {
+    document.getElementById("img").src = document.getElementById("picked").value;
+    }
+    // $(function(){
+    //     $("select").click(function(){
+    //         var flag = this.value;
+    //          $("#img").attr("src",flag);
+    //     });
+        
+        
+    // })
