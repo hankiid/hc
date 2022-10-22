92cf525d7abd3b8adadaa72172f3e73995d6f9e0
Pick_Summer <114598668+XZJY1229@users.noreply.github.com>
2022-10-22 19:36:53 +0800

    Create submit.js

---
+++ submit.js
@@ -0,0 +1,29 @@
+function changemy()
+{
+    document.querySelector('h3').innerHTML = '马克思主义原理' ;
+    document.getElementById('homework_content').innerHTML = '社会实践ppt的提交,文件命名的格式,页数不少于30页。' ;
+}
+
+function changevjf()
+{
+    document.querySelector('h3').innerHTML = '微积分' ;
+    document.getElementById('homework_content').innerHTML = '完成作业习题1 ~ 3。' ;
+}
+
+function changedw()
+{
+    document.querySelector('h3').innerHTML = '大学物理' ;
+    document.getElementById('homework_content').innerHTML = '完成作业习题T21 ~ T23，并完成附加作业。' ;
+}
+
+function changedy()
+{
+    document.gquerySelector('h3').innerHTML = '大学英语' ;
+    document.getElementById('homework_content').innerHTML = '完成书上练习，精听ted，并准备pre。' ;
+}
+
+function changemg()
+{
+    document.querySelector('h3').innerHTML = '毛概' ;
+    document.getElementById('homework_content').innerHTML = '简单介绍本课程给你带来的启发与思考，不少于两千字，并以小组为单位制作PPT上台展示。' ;
+}
