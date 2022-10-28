1f13ad2e9b4c5627998d7a8c11217a8cb4bbdae4
Ling521314 <114004522+Ling521314@users.noreply.github.com>
2022-10-28 19:22:57 +0800

    Update teacher.js

--- teacher.js
+++ teacher.js
@@ -1,11 +1,41 @@
-function show() {
-    document.getElementById("img").src = document.getElementById("picked").value;
-    }
-    // $(function(){
-    //     $("select").click(function(){
-    //         var flag = this.value;
-    //          $("#img").attr("src",flag);
-    //     });
-        
-        
-    // })
+var tishi = document.querySelector('.tishi')
+tishi.onclick = function () {
+    axios({
+        method: 'POST',
+        url: 'http://127.0.0.1/post/teacher/reminder'
+    }).then(value => {
+        console.log('成功')
+        console.log(value)
+    }, reason => {
+        console.log('失败')
+        console.log(reason.message)
+    })
+}
+
+var submit = document.querySelector('#submit')
+submit.onclick = function () {
+    axios({
+        method: 'POST',
+        url: 'http://127.0.0.1/post/putscore'
+    }).then(value => {
+        console.log('成功')
+        console.log(value)
+    }, reason => {
+        console.log('失败')
+        console.log(reason.message)
+    })
+}
+
+var submit2 = document.querySelector('#submit2')
+submit2.onclick = function () {
+    axios({
+        method: 'POST',
+        url: 'http://127.0.0.1/post/teacher/savescore'
+    }).then(value => {
+        console.log('成功')
+        console.log(value)
+    }, reason => {
+        console.log('失败')
+        console.log(reason.message)
+    })
+}
